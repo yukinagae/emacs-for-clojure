828bceaa43593c642461a2ac0ce0d0b8e425fdd5
Yuki Nagae <yuki.nagae1130@gmail.com>
2016-02-20 13:57:06 +1100

    web-mode for jsx

--- init.el
+++ init.el
@@ -183,6 +183,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$"     . web-mode))
 
 (defun web-mode-hook ()
    "Hooks for Web mode."
